f45ccd2b87b8304578d2d0d9016383f37acc66fd
AD <buituanbinat@gmail.com>
2023-05-23 09:51:29 +0700

    space

--- LeemEcommerce.Domain/Entity/Category.cs
+++ LeemEcommerce.Domain/Entity/Category.cs
@@ -9,7 +9,7 @@ namespace LeemEcommerce.Domain.Entity;
 public class Category : BaseModel
 {
     [Required][MaxLength(200)][MinLength(5)] public string Name { get; set; }
-    [MaxLength(1064)] public string? ImageUrl { get; set; }
+    [MaxLength(1064)] public string? ImageUrl { get; set; } 
     [MaxLength(1064)] public string? Url { get; set; }
     public bool IsActive { get; set; }
     public virtual ICollection<Product> Product { get; set; }
